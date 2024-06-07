8b27c22eafc6463385b99b3a44a3e4cdf0e9bb98
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-07 22:28:27 +0200

    added list of games

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,6 +1,30 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+  new (
+    1,
+    "GTA 5",
+    "Action",
+    25.5M,
+    new DateOnly(2013,7,15)),
+  new (
+    2,
+    "Mafia 2",
+    "Action",
+    20M,
+    new DateOnly(2010,7,15)),
+  new (
+    3,
+    "CS:GO",
+    "Action",
+    0M,
+    new DateOnly(1992,7,15)),
+];
+
+
+
 
 app.Run();
