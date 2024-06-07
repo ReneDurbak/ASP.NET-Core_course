026ec2182267592740ee6b4763531f52a25b6d94
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-07 22:49:23 +0200

    added delete endpoint

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -65,4 +65,12 @@ app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
     return Results.NoContent();
 });
 
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id) => {
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
+
 app.Run();
