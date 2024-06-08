0c3b106744e8cca5cf1f0dd3bdec20bc18a88f64
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-08 16:22:25 +0200

    implemented connection string to db from appsettings.json

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -3,7 +3,7 @@ using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
