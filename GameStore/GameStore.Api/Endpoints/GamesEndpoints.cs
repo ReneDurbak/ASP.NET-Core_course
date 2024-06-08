c5e4b5551ab4e3a09cfbf7c7b40c11b0e588f6b5
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-08 14:01:36 +0200

    added MapGamesEndpoints extension method

--- GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -26,4 +26,61 @@ public static class GamesEndpoints
         0M,
         new DateOnly(1992,7,15)),
     ];
+
+    public static WebApplication MapGamesEndpoints(this WebApplication app){
+        // GET /games
+        app.MapGet("games", () => games);
+
+        // GET /games/1
+        app.MapGet("games/{id}", (int id) => {
+            GameDto? game = games.Find(game => game.Id == id);
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        })
+        .WithName(GetGameEndpointName);
+
+        // POST /games
+        app.MapPost("games", (CreateGameDto newGame) => {
+            GameDto game = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+        });
+
+        // PUT /games/1
+        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
+
+            var index = games.FindIndex(game => game.Id == id);
+
+            if(index == -1){
+            return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+            id,
+            updatedGame.Name,
+            updatedGame.Genre,
+            updatedGame.Price,
+            updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        // DELETE /games/1
+        app.MapDelete("games/{id}", (int id) => {
+            games.RemoveAll(game => game.Id == id);
+
+            return Results.NoContent();
+        });
+        
+        return app;
+    }
 }
