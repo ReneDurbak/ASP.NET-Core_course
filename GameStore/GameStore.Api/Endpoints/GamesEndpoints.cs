d574c876a8f54f859793e99c02b034762312a364
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-08 14:00:09 +0200

    created GamesEndpoints static class

---
+++ GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,29 @@
+﻿using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGame";
+
+    private static readonly List<GameDto> games = [
+    new (
+        1,
+        "GTA 5",
+        "Action",
+        25.5M,
+        new DateOnly(2013,7,15)),
+    new (
+        2,
+        "Mafia 2",
+        "Action",
+        20M,
+        new DateOnly(2010,7,15)),
+    new (
+        3,
+        "CS:GO",
+        "Action",
+        0M,
+        new DateOnly(1992,7,15)),
+    ];
+}
