17f6c3c09b63c9886eaabe18dafccb2ad2afb480
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-08 14:09:21 +0200

    added route group for endpoints

--- GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -27,12 +27,15 @@ public static class GamesEndpoints
         new DateOnly(1992,7,15)),
     ];
 
-    public static WebApplication MapGamesEndpoints(this WebApplication app){
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app){
+
+        var group = app.MapGroup("games");
+
         // GET /games
-        app.MapGet("games", () => games);
+        group.MapGet("/", () => games);
 
         // GET /games/1
-        app.MapGet("games/{id}", (int id) => {
+        group.MapGet("/{id}", (int id) => {
             GameDto? game = games.Find(game => game.Id == id);
 
             return game is null ? Results.NotFound() : Results.Ok(game);
@@ -40,7 +43,7 @@ public static class GamesEndpoints
         .WithName(GetGameEndpointName);
 
         // POST /games
-        app.MapPost("games", (CreateGameDto newGame) => {
+        group.MapPost("/", (CreateGameDto newGame) => {
             GameDto game = new(
                 games.Count + 1,
                 newGame.Name,
@@ -55,7 +58,7 @@ public static class GamesEndpoints
         });
 
         // PUT /games/1
-        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) => {
 
             var index = games.FindIndex(game => game.Id == id);
 
@@ -75,12 +78,12 @@ public static class GamesEndpoints
         });
 
         // DELETE /games/1
-        app.MapDelete("games/{id}", (int id) => {
+        group.MapDelete("/{id}", (int id) => {
             games.RemoveAll(game => game.Id == id);
 
             return Results.NoContent();
         });
         
-        return app;
+        return group;
     }
 }
