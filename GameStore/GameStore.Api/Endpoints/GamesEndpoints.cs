f356e8175a7f9c742a8c7f2e72faf26ebd7fb750
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-10 15:12:20 +0200

    updated POST request

--- GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,4 +1,6 @@
-﻿using GameStore.Api.Dtos;
+﻿using GameStore.Api.Data;
+using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
 
 namespace GameStore.Api.Endpoints;
 
@@ -43,19 +45,28 @@ public static class GamesEndpoints
         .WithName(GetGameEndpointName);
 
         // POST /games
-        group.MapPost("/", (CreateGameDto newGame) => {
+        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) => {
 
-            GameDto game = new(
-                games.Count + 1,
-                newGame.Name,
-                newGame.Genre,
-                newGame.Price,
-                newGame.ReleaseDate
+            Game game = new(){
+                Name = newGame.Name,
+                Genre = dbContext.Genres.Find(newGame.GenreId),
+                GenreId = newGame.GenreId,
+                Price = newGame.Price,
+                ReleaseDate = newGame.ReleaseDate
+            };
+
+            dbContext.Games.Add(game);
+            dbContext.SaveChanges();
+
+            GameDto gameDto = new(
+                game.Id,
+                game.Name,
+                game.Genre!.Name, // !(null forgiving operator) - Genre is never going to be null
+                game.Price,
+                game.ReleaseDate
             );
 
-            games.Add(game);
-
-            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, gameDto);
         });
 
         // PUT /games/1
