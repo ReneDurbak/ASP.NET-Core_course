cfb5bcb6574eb3e2700032b4ea38e22cca714563
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-10 15:43:28 +0200

    GameMapping static class created

---
+++ GameStore/GameStore.Api/Mapping/GameMapping.cs
@@ -0,0 +1,27 @@
+﻿using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Mapping;
+
+public static class GameMapping
+{
+    public static Game ToEntity(this CreateGameDto game){
+         return new Game(){
+                Name = game.Name,
+                GenreId = game.GenreId,
+                Price = game.Price,
+                ReleaseDate = game.ReleaseDate
+            };
+    }
+
+    public static GameDto ToDto(this Game game){
+        return new(
+            game.Id,
+            game.Name,
+            game.Genre!.Name, // !(null forgiving operator) - Genre is never going to be null
+            game.Price,
+            game.ReleaseDate
+        );
+
+    }
+}
