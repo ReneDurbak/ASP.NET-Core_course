e48b6872ba4a9adbc5ef2420fcc6d1285e6924e1
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-10 16:42:35 +0200

    updated single get endpoint and implemented new DTO's

--- GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -9,7 +9,7 @@ public static class GamesEndpoints
 {
     const string GetGameEndpointName = "GetGame";
 
-    private static readonly List<GameDto> games = [
+    private static readonly List<GameSummaryDto> games = [
     new (
         1,
         "GTA 5",
@@ -38,27 +38,24 @@ public static class GamesEndpoints
         group.MapGet("/", () => games);
 
         // GET /games/1
-        group.MapGet("/{id}", (int id) => {
-            GameDto? game = games.Find(game => game.Id == id);
+        group.MapGet("/{id}", (int id, GameStoreContext dbContext) => {
+            Game? game = dbContext.Games.Find(id);
 
-            return game is null ? Results.NotFound() : Results.Ok(game);
+            return game is null ? Results.NotFound() : Results.Ok(game.ToGameDetailsDto());
         })
         .WithName(GetGameEndpointName);
 
         // POST /games
         group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) => {
             Game game = newGame.ToEntity();
-            game.Genre = dbContext.Genres.Find(newGame.GenreId);
-
 
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
 
-
             return Results.CreatedAtRoute(
                 GetGameEndpointName, 
                 new {id = game.Id}, 
-                game.ToDto()
+                game.ToGameDetailsDto()
             );
         });
 
@@ -71,7 +68,7 @@ public static class GamesEndpoints
             return Results.NotFound();
             }
 
-            games[index] = new GameDto(
+            games[index] = new GameSummaryDto(
             id,
             updatedGame.Name,
             updatedGame.Genre,
