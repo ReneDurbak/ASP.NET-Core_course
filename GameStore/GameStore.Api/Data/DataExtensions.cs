68a31a04956b8be3ebf2eee5f7f72ddb0b16b1ff
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-06-11 08:06:34 +0200

    Updated MigrateDb function to async

--- GameStore/GameStore.Api/Data/DataExtensions.cs
+++ GameStore/GameStore.Api/Data/DataExtensions.cs
@@ -4,9 +4,9 @@ namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
-    public static void MigrateDb(this WebApplication app){
+    public static async Task MigrateDbAsync(this WebApplication app){
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
-        dbContext.Database.Migrate();
+        await dbContext.Database.MigrateAsync();
     }
 }
